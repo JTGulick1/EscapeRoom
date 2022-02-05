cf80fcc72ba4a642416153b84419e9973f3389a3
jtgulick1 <jtgulick1@gmail.com>
2022-02-05 14:58:08 -0500

    Safe Changes

--- Assets/Scripts/NumLock.cs
+++ Assets/Scripts/NumLock.cs
@@ -27,44 +27,44 @@ public class NumLock : MonoBehaviour
     {
         if (name == "num1")
         {
-            if (num1 != 9)
+            if (num1 != 10)
             {
                 num1++;
             }
-            if (num1 == 9)
+            if (num1 == 10)
             {
                 num1 = 0;
             }
         }
         if (name == "num2")
         {
-            if (num2 != 9)
+            if (num2 != 10)
             {
                 num2++;
             }
-            if (num2 == 9)
+            if (num2 == 10)
             {
                 num2 = 0;
             }
         }
         if (name == "num3")
         {
-            if (num3 != 9)
+            if (num3 != 10)
             {
                 num3++;
             }
-            if (num3 == 9)
+            if (num3 == 10)
             {
                 num3 = 0;
             }
         }
         if (name == "num4")
         {
-            if (num4 != 9)
+            if (num4 != 10)
             {
                 num4++;
             }
-            if (num4 == 9)
+            if (num4 == 10)
             {
                 num4 = 0;
             }
